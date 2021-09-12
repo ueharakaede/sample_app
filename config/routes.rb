f8645d3bed68f23c215af534254ee797247fd4d6
EC2 Default User <ec2-user@ip-172-31-26-131.ec2.internal>
2021-09-12 12:37:08 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'todolists/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+  post 'todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#index'
+  get 'todolists/:id' => 'todolists#show', as: 'todolist'
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+end
